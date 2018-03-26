cb116ad23d57b132971b8a1b8d35fa89f3cd3d31
Unknown <JonathanILevi@gmail.com>
2018-03-26 17:28:38 -0500

    Added socket_msg_queue.

---
+++ src/socket_msg_queue.d
@@ -0,0 +1,97 @@
+/**
+MsgQueue takes a socket and recieves and converts the buffer to mesages to individual mesages.
+MsgQueue assumes that the first byte of a msg is the mesages length + 3(for msg header).
+*/
+
+
+module socket_msg_queue	;
+
+import cst_;
+import queue	;
+
+import core.thread	;
+import std.socket	;
+
+
+
+class SocketMsgQueue {
+	private {
+		Socket socket	;
+		Queue!(ubyte[]) queue	;
+		
+		MsgThread msgThread;
+		
+		shared bool _closed = false;
+	}
+	
+	this(Socket socket) {
+		this.socket	= socket	;
+		this.queue	= new Queue!(ubyte[])	;
+				
+		this.msgThread	= new MsgThread(socket, queue, _closed)	;
+
+		socket.blocking = true;
+		
+		msgThread.start();
+	}
+	
+	public bool closed() @property {
+		return _closed;
+	}
+	
+	auto empty()	{ return queue.empty	;	}
+	auto popFront()	{ return queue.popFront	;	}
+	auto front()	{ return queue.front	;	}
+}
+
+private class MsgThread : Thread {
+	private:
+
+	public this(Socket socket, Queue!(ubyte[]) queue, shared bool _closed) {
+		this.socket	= socket	;
+		this.queue	= queue	;
+		this._closed	= _closed	;
+
+		super(&run);
+	}
+	
+	Socket	socket	;
+	Queue!(ubyte[])	queue	;
+	shared bool	_closed	;
+			
+	ubyte[258]	buffer	;// 258 = 255+3  (max msg size (ubyte) plus header)
+	ubyte[]	partialMsg	;
+	uint	readLength	;// The current length of the read data.
+	
+	private void run() {
+		while (true) {
+			import core.time : msecs;
+			Thread.sleep(msecs(60));
+
+			ptrdiff_t length = socket.receive(buffer[readLength..$]);
+			if (length==0 || length==Socket.ERROR) {
+				_closed = true;
+				continue;
+			}
+			////readLength += length;
+			////import std.math : min;
+			partialMsg ~= buffer[0..length];
+			
+			if (partialMsg.length >= partialMsg[0]+3) {//+3 for header
+				queue.put(partialMsg[0..partialMsg[0]+3]);
+				partialMsg = partialMsg[partialMsg[0]+3..$];
+			}
+		}
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
