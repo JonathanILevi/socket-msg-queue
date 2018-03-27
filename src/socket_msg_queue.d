5895f03eb76224b8c2902efbef5ff638a6ae0774
Unknown <JonathanILevi@gmail.com>
2018-03-26 21:08:39 -0500

    Added a callback for geting msg length.

--- src/socket_msg_queue.d
+++ src/socket_msg_queue.d
@@ -25,11 +25,11 @@ class SocketMsgQueue {
 		shared bool _closed = false;
 	}
 	
-	this(Socket socket) {
+	this(Socket socket, ptrdiff_t function(ubyte[]) getMsgLength) {
 		this.socket	= socket	;
 		this.queue	= new Queue!(ubyte[])	;
 				
-		this.msgThread	= new MsgThread(socket, queue, _closed)	;
+		this.msgThread	= new MsgThread(socket, queue, _closed, getMsgLength)	;
 
 		socket.blocking = true;
 		
@@ -48,10 +48,12 @@ class SocketMsgQueue {
 private class MsgThread : Thread {
 	private:
 
-	public this(Socket socket, Queue!(ubyte[]) queue, ref shared bool _closed) {
+	public this(Socket socket, Queue!(ubyte[]) queue, ref shared bool _closed, ptrdiff_t function(ubyte[]) getMsgLength) {
 		this.socket	= socket	;
 		this.queue	= queue	;
 		this._closed	= &_closed	;
+		
+		this.getMsgLength = getMsgLength;
 
 		super(&run);
 	}
@@ -59,7 +61,9 @@ private class MsgThread : Thread {
 	Socket	socket	;
 	Queue!(ubyte[])	queue	;
 	shared bool*	_closed	;
-			
+	
+	ptrdiff_t function(ubyte[]) getMsgLength;
+	
 	ubyte[258]	buffer	;// 258 = 255+3  (max msg size (ubyte) plus header)
 	ubyte[]	partialMsg	;
 	uint	readLength	;// The current length of the read data.
@@ -77,7 +81,7 @@ private class MsgThread : Thread {
 			////import std.math : min;
 			partialMsg ~= buffer[0..length];
 			
-			if (partialMsg.length >= partialMsg[0]+3) {//+3 for header
+			if (partialMsg.length >= getMsgLength(partialMsg)) {
 				queue.put(partialMsg[0..partialMsg[0]+3]);
 				partialMsg = partialMsg[partialMsg[0]+3..$];
 			}
